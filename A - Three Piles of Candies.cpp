d2cc2c78f870c3548f4d990312a88e1d8bbaad0e
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-02-26 02:02:35 +0600

    implementation

---
+++ A - Three Piles of Candies.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long a,b,c,sum;
+        cin>>a>>b>>c;
+        sum=(a+b+c)/2;
+ 
+        //long long int sum1=(sum-48)/2;
+        cout<<sum<<endl;
+    }
+ 
+    return 0;
+}
