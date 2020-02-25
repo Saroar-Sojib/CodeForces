d5469882b3d78b69663df66d114f22927120fed6
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-02-26 01:42:39 +0600

    implementation

---
+++ A - Theatre Square.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,m,a;
+    cin>>n>>m>>a;
+    long long int p=((n+a-1)/a)*((m+a-1)/a);
+ 
+ 
+    cout<<p<<endl;
+    return 0;
+}
