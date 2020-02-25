530eeb6a26a5aa3f82ba7b2576e85bb85ed3726b
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-02-26 01:55:44 +0600

    implementation

---
+++ A - Remove a Progression.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+#define soji(n) for(int i=0; i<n; i++)
+#define sojib(j,n) for(int i=j; i<n; i++)
+ 
+typedef long long ll;
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    soji(t)
+    {
+        ll n, x;
+        cin>>n>>x;
+        cout<<2*x<<endl;
+    }
+ 
+ 
+    return 0;
+}
