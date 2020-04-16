0cbcec45156b23c123de54ac1613f601655d0377
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-04-16 13:31:15 +0600

    Implementation

---
+++ A - Ichihime and Triangle.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+         long long a,b,c,d;
+           cin>>a>>b>>c>>d;
+           cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+ 
+ 
+return 0;
+ 
+ 
+ 
+}
