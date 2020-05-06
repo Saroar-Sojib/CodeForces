0841183012b2344b7b8f4f39cb6b45088c42f54d
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-05-06 20:20:53 +0600

    brute force, dp, implementation, math

---
+++ A - Filling Diamonds.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
